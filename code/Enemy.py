72a7e4e6174a89589d83c605fd08e973a072e9da
Elias Moreira <elias.moreira@ipdec.org>
2024-11-04 16:47:35 -0300

    feat: level 3 implementation

--- code/Enemy.py
+++ code/Enemy.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-from code.Const import ENTITY_SPEED, ENTITY_SHOT_DELAY
+from code.Const import ENTITY_SPEED, ENTITY_SHOT_DELAY, WIN_HEIGHT
 from code.EnemyShot import EnemyShot
 from code.Entity import Entity
 
@@ -9,9 +9,20 @@ class Enemy(Entity):
     def __init__(self, name: str, position: tuple):
         super().__init__(name, position)
         self.shot_delay = ENTITY_SHOT_DELAY[self.name]
+        self.rising = True
 
     def move(self):
         self.rect.centerx -= ENTITY_SPEED[self.name]
+        if self.rising:
+            if self.rect.top > 0:
+                self.rect.centery -= ENTITY_SPEED[self.name]
+            if self.rect.top == 0 or self.rect.top < 0:
+                self.rising = False
+        else:
+            if self.rect.bottom < WIN_HEIGHT:
+                self.rect.centery += ENTITY_SPEED[self.name] * 2
+            if self.rect.bottom == WIN_HEIGHT or self.rect.bottom > WIN_HEIGHT:
+                self.rising = True
 
     def shoot(self):
         self.shot_delay -= 1
